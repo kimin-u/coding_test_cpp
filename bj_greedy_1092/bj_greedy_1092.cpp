152577ffab55f650e939dda56b5d5ab01104b6a7
kimin-u <k384077@gmail.com>
2024-02-01 21:06:09 +0900

    bj_greedy_1092_그리디_인터넷참고,,

---
+++ bj_greedy_1092/bj_greedy_1092.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+
+bool compare(int a, int b) {
+	if (a > b) return true;
+	return false;
+}
+
+int main(void) {
+	cin >> n;
+	vector<int> crain(n);
+	for (int i = 0; i < n; i++) {
+		cin >> crain[i];
+	}
+	
+	cin >> m;
+	vector<int> box(m);
+	for (int i = 0; i < m; i++) {
+		cin >> box[i];
+	}
+
+	int time = 0;
+	
+	sort(crain.begin(), crain.end(), compare);
+	sort(box.begin(), box.end(), compare);
+
+	if (box[0] > crain[0]) {
+		cout << -1; 
+		return 0;
+	}
+
+	while (true) {
+		int index = 0;
+		for (int i = 0; i < m; i++) {
+			if (crain[index] > box[i]) {
+				index++;
+				box.erase(box.begin() + i);
+				m--;
+				i--;
+			}
+		}
+		time++;
+	}
+
+	cout << time;
+	return 0;
+}
